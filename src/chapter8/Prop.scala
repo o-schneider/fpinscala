70bc45b64c64e96967c5b9ee762e6285d59bfbfe
Olivier Schneider <olivier.schneider@opengroupware.ch>
2016-11-06 21:35:17 +0100

    8.5

--- src/chapter8/Prop.scala
+++ src/chapter8/Prop.scala
@@ -27,6 +27,13 @@ object Gen {
     })
     Gen(range)
   }
+
+  def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
+
+  def boolean: Gen[Boolean] = Gen(RNG.int.map(i => i > 0))
+
+  def listofN[A](n: Int, g: Gen[A]): Gen[List[A]] = Gen(State.sequence(List.fill(n)(g.sample)))
+
 }
 
 object Main {
