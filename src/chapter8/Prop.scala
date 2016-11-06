736680d9eb573724c26afb03782f37e9f88afba1
Olivier Schneider <olivier.schneider@opengroupware.ch>
2016-11-06 21:28:08 +0100

    8.4

--- src/chapter8/Prop.scala
+++ src/chapter8/Prop.scala
@@ -1,9 +1,41 @@
 package chapter8
 
-trait Prop {
-  def check(): Boolean
+import java.awt.image.SampleModel
 
-  def &&(p: Prop): Prop = () => check() && p.check()
+import chapter6.RNG.Rand
+import chapter6.{RNG, SimpleRNG, State}
+import chapter8.Prop.{FailedCase, SuccessCount}
+
+object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
 }
 
+trait Prop {
+  def check(): Either[(FailedCase, SuccessCount), SuccessCount]
 
+  //def &&(p: Prop): Prop = () => check() && p.check()
+}
+
+case class Gen[A](sample: Rand[A])
+
+object Gen {
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    lazy val range: Rand[Int] = RNG.int.flatMap(i => {
+      val mod = i % stopExclusive
+      if (i + (stopExclusive - 1) - mod >= start) State.unit(mod) else range
+    })
+    Gen(range)
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val rng = SimpleRNG(42)
+    val gen = Gen.choose(-1, 100)
+    val run1 = gen.sample.run(rng)
+    val run2 = gen.sample.run(run1._2)
+    println(run1._1)
+    println(run2._1)
+  }
+}
